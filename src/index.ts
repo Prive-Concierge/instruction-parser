e0310d102796cfb0ff46d40297d179fd5f70a7aa
Onyemaechi Okafor <onyemaechiokafor@Onyemaechis-MacBook-Pro.local>
2026-01-31 10:39:23 +0100

    remove price

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { unpackAccount, unpackMint } from "@solana/spl-token";
 import { AccountInfo, Connection, PublicKey } from "@solana/web3.js";
 import Decimal from "decimal.js";
 import { InstructionParser } from "./lib/instruction-parser";
-import { DecimalUtil, getPriceInUSDByMint } from "./lib/utils";
+import { DecimalUtil } from "./lib/utils";
 import { getEvents } from "./lib/get-events";
 import { AMM_TYPES, JUPITER_V6_PROGRAM_ID } from "./constants";
 import { FeeEvent, SwapEvent, TransactionWithMeta } from "./types";
@@ -26,29 +26,23 @@ export type SwapAttributes = {
   signature: string;
   timestamp: Date;
   legCount: number;
-  volumeInUSD: number;
   inSymbol: string;
   inAmount: BigInt;
   inAmountInDecimal?: number;
-  inAmountInUSD: number;
   inMint: string;
   outSymbol: string;
   outAmount: BigInt;
   outAmountInDecimal?: number;
-  outAmountInUSD: number;
   outMint: string;
   instruction: string;
   exactInAmount: BigInt;
-  exactInAmountInUSD: number;
   exactOutAmount: BigInt;
-  exactOutAmountInUSD: number;
   swapData: JSON;
   feeTokenPubkey?: string;
   feeOwner?: string;
   feeSymbol?: string;
   feeAmount?: BigInt;
   feeAmountInDecimal?: number;
-  feeAmountInUSD?: number;
   feeMint?: string;
   tokenLedger?: string;
   lastAccount: string; // This can be a tracking account since we don't have a way to know we just log it the last account.
@@ -119,9 +113,6 @@ export async function extract(
   const inAmountInDecimal = inSwapData.reduce((acc, curr) => {
     return acc.add(curr.inAmountInDecimal ?? 0);
   }, new Decimal(0));
-  const inAmountInUSD = inSwapData.reduce((acc, curr) => {
-    return acc.add(curr.inAmountInUSD ?? 0);
-  }, new Decimal(0));
 
   const outSymbol = null; // We don't longer support this.
   const outMint = swapData[finalPositions[0]].outMint;
@@ -134,14 +125,6 @@ export async function extract(
   const outAmountInDecimal = outSwapData.reduce((acc, curr) => {
     return acc.add(curr.outAmountInDecimal ?? 0);
   }, new Decimal(0));
-  const outAmountInUSD = outSwapData.reduce((acc, curr) => {
-    return acc.add(curr.outAmountInUSD ?? 0);
-  }, new Decimal(0));
-
-  const volumeInUSD =
-    outAmountInUSD && inAmountInUSD
-      ? Decimal.min(outAmountInUSD, inAmountInUSD)
-      : outAmountInUSD ?? inAmountInUSD;
 
   const swap = {} as SwapAttributes;
 
@@ -156,18 +139,15 @@ export async function extract(
   swap.signature = signature;
   swap.timestamp = new Date(new Date((blockTime ?? 0) * 1000).toISOString());
   swap.legCount = swapEvents.length;
-  swap.volumeInUSD = volumeInUSD.toNumber();
 
   swap.inSymbol = inSymbol;
   swap.inAmount = inAmount;
   swap.inAmountInDecimal = inAmountInDecimal.toNumber();
-  swap.inAmountInUSD = inAmountInUSD.toNumber();
   swap.inMint = inMint;
 
   swap.outSymbol = outSymbol;
   swap.outAmount = outAmount;
   swap.outAmountInDecimal = outAmountInDecimal.toNumber();
-  swap.outAmountInUSD = outAmountInUSD.toNumber();
   swap.outMint = outMint;
 
   const exactOutAmount = parser.getExactOutAmount(
@@ -175,13 +155,6 @@ export async function extract(
   );
   if (exactOutAmount) {
     swap.exactOutAmount = BigInt(exactOutAmount);
-
-    if (outAmountInUSD) {
-      swap.exactOutAmountInUSD = new Decimal(exactOutAmount)
-        .div(outAmount.toString())
-        .mul(outAmountInUSD)
-        .toNumber();
-    }
   }
 
   const exactInAmount = parser.getExactInAmount(
@@ -189,19 +162,12 @@ export async function extract(
   );
   if (exactInAmount) {
     swap.exactInAmount = BigInt(exactInAmount);
-
-    if (inAmountInUSD) {
-      swap.exactInAmountInUSD = new Decimal(exactInAmount)
-        .div(inAmount.toString())
-        .mul(inAmountInUSD)
-        .toNumber();
-    }
   }
 
   swap.swapData = JSON.parse(JSON.stringify(swapData));
 
   if (feeEvent) {
-    const { mint, amount, amountInDecimal, amountInUSD } = await extractVolume(
+    const { mint, amount, amountInDecimal } = await extractVolume(
       accountInfosMap,
       feeEvent.mint,
       feeEvent.amount
@@ -213,7 +179,6 @@ export async function extract(
     )?.toBase58();
     swap.feeAmount = BigInt(amount);
     swap.feeAmountInDecimal = amountInDecimal?.toNumber();
-    swap.feeAmountInUSD = amountInUSD?.toNumber();
     swap.feeMint = mint;
   }
 
@@ -243,7 +208,6 @@ async function extractSwapData(
     mint: inMint,
     amount: inAmount,
     amountInDecimal: inAmountInDecimal,
-    amountInUSD: inAmountInUSD,
   } = await extractVolume(
     accountInfosMap,
     swapEvent.inputMint,
@@ -253,7 +217,6 @@ async function extractSwapData(
     mint: outMint,
     amount: outAmount,
     amountInDecimal: outAmountInDecimal,
-    amountInUSD: outAmountInUSD,
   } = await extractVolume(
     accountInfosMap,
     swapEvent.outputMint,
@@ -265,11 +228,9 @@ async function extractSwapData(
     inMint,
     inAmount,
     inAmountInDecimal,
-    inAmountInUSD,
     outMint,
     outAmount,
     outAmountInDecimal,
-    outAmountInUSD,
   };
 }
 
@@ -278,18 +239,12 @@ async function extractVolume(
   mint: PublicKey,
   amount: BN
 ) {
-  const tokenPriceInUSD = await getPriceInUSDByMint(mint.toBase58());
   const tokenDecimals = extractMintDecimals(accountInfosMap, mint);
   const amountInDecimal = DecimalUtil.fromBN(amount, tokenDecimals);
-  const amountInUSD = tokenPriceInUSD
-    ? amountInDecimal.mul(tokenPriceInUSD)
-    : undefined;
-
   return {
     mint: mint.toBase58(),
     amount: amount.toString(),
     amountInDecimal,
-    amountInUSD,
   };
 }
 
